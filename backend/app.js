8bd35eb5a2cc62541a0702eda39ed24e859bde21
Indira Gothi <indiragothi007@gmail.com>
2024-07-10 12:36:38 +0530

    start edit

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const path = require('path')
 
 const connectToMongoDB = require('./db/connectToMongoDB')
 const settingRoute = require('./routes/settingRoute')
@@ -10,6 +11,9 @@ const PORT = process.env.PORT || 3000;
 
 dotenv.config();
 
+const __dirname = path.resolve();
+
+
 app.use(cors({
     origin: ["http://localhost:3001/", "https://fog-animated-grid.onrender.com/"]
 }));
@@ -17,6 +21,13 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/v1/', settingRoute)
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 
 app.listen(PORT, () =>{
     connectToMongoDB();
